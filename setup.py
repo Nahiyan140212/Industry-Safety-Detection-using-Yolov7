7b0696372b43796e916d52c0156d76be93ac5f61
Nahiyan140212 <nbnoor@ualr.edu>
2024-03-20 09:13:36 -0500

    Logging, Exception, utils updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'Nahiyan',
+    author_email= 'nbnoor@ualr.edu',
+    packages= find_packages(),
+    install_requires = []
+
+)
